cfe652254ee8445d67f9a80f4687cfd70a03cfa2
Paulo Ricardo Horta Grando <paulorhg@hotmail.com>
2020-02-10 17:19:57 -0300

    Add files via upload

---
+++ Pilha.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Pilha
+{
+    private object[] elementos;
+    private int topo, max;
+
+    public Pilha(int maximo)
+    {
+        max = maximo;
+        elementos = new object[max];
+        topo = 0;
+    }
+
+    public void Push(object x) {
+        if (PilhaCheia())
+            throw new Exception("Pilha Cheia!");
+
+        elementos[topo++] = x;
+    }
+
+    public object Pop() {
+        if (PilhaVazia())
+            throw new Exception("Pilha Vazia!");
+
+        return elementos[--topo];
+    }
+
+    public bool PilhaCheia()
+    {
+        return topo == max;
+    }
+
+    public bool PilhaVazia()
+    {
+        return topo == 0;
+    }
+
+}
